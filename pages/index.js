29a6130ee8b0cc7b07786f7bc3c4d70409f5f48a
Alexander Castillo <jhuiicebox@gmail.com>
2022-09-24 18:27:47 -0600

    encorporated getStaticProps server side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
+
+import { getSortedPostsData } from '../lib/posts';
+
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -17,6 +20,29 @@ export default function Home() {
         </p>
         <Link href="/posts/first-post"> Check out my posts</Link>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title}) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
