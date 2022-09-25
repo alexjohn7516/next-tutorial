dd359c7d4671b4626ca9f326a46e3633c2836d45
Alexander Castillo <jhuiicebox@gmail.com>
2022-09-25 14:23:24 -0600

    reformated posts/[id] HTML and css

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,19 +1,26 @@
 import Layout from "../../components/layout";
+import Date from "../../components/date";
+import Head from "next/head";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
 
 //* [1 of 3]
 //* Destructure params value postData from getStaticProps
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      {/* ? a div with a closing tage? */}
-      <div dangerouslySetInnerHTML={{__html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        {/* {postData.id} */}
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        {/* ? a div with a closing tage? */}
+        <div dangerouslySetInnerHTML={{__html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
